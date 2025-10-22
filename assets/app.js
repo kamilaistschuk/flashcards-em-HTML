4bb95c15db7b8c251749a8817546906e2e4a0d15
kamilaistschuk <kamila.malamim@escola.pr.gov.br>
2025-10-22 08:51:37 -0300

    .

---
+++ assets/app.js
@@ -0,0 +1,5 @@
+function criaCartao (categoria, pergunta, resposta){
+    let container = document.getElementById('container')
+    let cartao = document.criateElement('article') 
+    cartao.classname = 'cartao'
+}
