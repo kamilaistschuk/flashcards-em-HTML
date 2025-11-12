63bb68c31c98304ab48b7adeedc7344b0537b19d
kamilaistschuk <kamila.malamim@escola.pr.gov.br>
2025-11-12 08:05:40 -0300

    .

--- assets/app.js
+++ assets/app.js
@@ -13,3 +13,11 @@ cartao.innerHTML='
     <p>${resposta}</p>
 </div>
 </div>
+let respostaEstaVisivel = false
+function viraCartao() {
+    respostaEstaVisivel =!respostaEstaVisivel
+    cartao.classList.toggle('active',respostaEstaVisivel)
+}
+cartao.addEventListener('click', viraCartao)
+container.appendChild(cartao)
+}
